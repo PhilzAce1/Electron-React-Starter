f53533a128f89e3f0bb74daa7beec903b3ffe4a5
Philzace1 <unknownumeh@gmail.com>
2021-03-11 03:23:44 +0100

    first commit

---
+++ public/electron.js
@@ -0,0 +1,34 @@
+const electron = require('electron'); // import electron package
+const app = electron.app; // assign the electron 'app' method to the app constant
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 1020, height: 720 });
+  mainWindow.setMenuBarVisibility(false); // display menu
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+  mainWindow.on('closed', () => (mainWindow = null));
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
